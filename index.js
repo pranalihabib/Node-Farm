16d35af5469841ee3bcb18be85af6462fa8242de
Pranali Habib <pranalihabib@gmail.com>
2025-12-22 19:59:15 +0530

    deprecated warning removed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http')
-const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 //////////////////
 // FILES
@@ -32,21 +32,6 @@ const url = require('url')
 ////////////////////
 // SERVER
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-    return output;
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -55,7 +40,9 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
+    const myURL = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = myURL.pathname;
+    const query = Object.fromEntries(myURL.searchParams);
 
     // Overview page
     if(pathname === '/' || pathname === '/overview'){ 
